28bda0cc1e5e523151e1beec8f4d8f25e9df96df
ArvidBrugman <arvid.brugman@student.uva.nl>
2024-11-15 14:31:07 +0100

    trying to build in the steps between 0 and 2pi commando

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -10,18 +10,34 @@ def cmd_group():
 
 @cmd_group.command()
 @click.argument("number", type=int)
-def sin(number):
-    x = np.linspace(0, 2 * pi, number)
-    df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
+@click.option(
+    "-n",
+    "--steps",
+    default=1,
+    help="Number of steps between 0 and 2pi.",
+    show_default=True,  # show default in help
+)
+def sin(number,steps):
+    for _ in range(steps):
+        x = np.linspace(0, 2 * pi, number)
+        df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
 @cmd_group.command()
 @click.argument("number", type=int)
-def tan(number):
-    x = np.linspace(0, 2 * pi, number)
-    df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
-    print(df)
+@click.option(
+    "-n",
+    "--steps",
+    default=1,
+    help="Number of steps between 0 and 2pi.",
+    show_default=True,  # show default in help
+)
+def tan(number,steps):
+    for _ in range(steps):
+        x = np.linspace(0, 2 * pi, number)
+        df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
+        print(df)
 
 
 if __name__ == "__main__":
-    sin(10)
+    sin(10) #cmd_group()
