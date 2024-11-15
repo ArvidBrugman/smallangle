a8a2afd507f33dba062fe24f05c776907c4656c6
ArvidBrugman <arvid.brugman@student.uva.nl>
2024-11-15 14:40:11 +0100

    made working steps and -n commando

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -9,31 +9,29 @@ def cmd_group():
     pass
 
 @cmd_group.command()
-@click.argument("number", type=int)
 @click.option(
     "-n",
-    "--steps",
+    "--number",
     default=1,
     help="Number of steps between 0 and 2pi.",
     show_default=True,  # show default in help
 )
-def sin(number,steps):
-    for _ in range(steps):
+def sin(number):
+    for _ in range(number):
         x = np.linspace(0, 2 * pi, number)
         df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
 @cmd_group.command()
-@click.argument("number", type=int)
 @click.option(
     "-n",
-    "--steps",
+    "--number",
     default=1,
     help="Number of steps between 0 and 2pi.",
     show_default=True,  # show default in help
 )
-def tan(number,steps):
-    for _ in range(steps):
+def tan(number):
+    for _ in range(number):
         x = np.linspace(0, 2 * pi, number)
         df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
         print(df)
