5f1ccbe8c96099b9f64ea71caef4de9a5ead8161
ArvidBrugman <arvid.brugman@student.uva.nl>
2024-11-15 14:15:06 +0100

    made subcommando's for sin and cos with the commando smallangle

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -4,12 +4,19 @@ from numpy import pi
 import pandas as pd
 
 
+@click.group()
+def cmd_group():
+    pass
+
+@cmd_group.command()
+@click.argument("number", type=int)
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
-
+@cmd_group.command()
+@click.argument("number", type=int)
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
